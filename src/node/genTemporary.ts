1c79c746ff1ee5aeef05eaa9b94ef6ead0fe8959
dewfall123 <1334767234@qq.com>
2020-11-18 23:28:20 +0800

    feat: support locale

--- src/node/genTemporary.ts
+++ src/node/genTemporary.ts
@@ -1,18 +1,26 @@
 import globby from 'globby'
 import fsExtra from 'fs-extra'
 import { cachedRead, ServerConfig } from 'vite'
-import { basename, join } from 'path'
+import { basename, extname, join } from 'path'
 import { resolveConfig } from './'
 import matter from 'gray-matter'
 import { clearSuffix } from './utils/parseHeader'
+import { LocaleConfig } from '/@types/shared'
 
 export const TempFileName = '.temp'
 export const DefaultSrcIncludes = ['src']
 
+// generate a .temp dir
 export async function genTemporary(options: ServerConfig = {}) {
   const root = options.root ?? process.cwd()
   const realRoot = join(root, TempFileName)
   const config = await resolveConfig(root)
+  const userConfig = config.userConfig
+  const langToPathMapping = getLangToPathMapping(
+    userConfig.locales ?? userConfig.themeConfig?.locales,
+    config.userConfig.lang
+  )
+
   const {
     userConfig: { srcIncludes: srcIncludes = DefaultSrcIncludes }
   } = config
@@ -20,23 +28,39 @@ export async function genTemporary(options: ServerConfig = {}) {
   await fsExtra.remove(realRoot)
   await fsExtra.ensureDir(realRoot)
 
-  await Promise.all([copyDocs(root), copySrc(root, srcIncludes)])
+  await Promise.all([
+    copyDocs(root, langToPathMapping),
+    copySrc(root, srcIncludes, langToPathMapping)
+  ])
 }
 
 // copy all file at root to .temp dir
-async function copyDocs(root: string) {
+async function copyDocs(
+  root: string,
+  langToPathMapping: Record<string, string> | null
+) {
   const files = await globby([`**`, `!${TempFileName}`], {
     dot: true,
     cwd: root
   })
   files.forEach((file) => {
-    fsExtra.copy(join(root, file), join(root, TempFileName, file))
+    const descFile = join(
+      root,
+      TempFileName,
+      tolocalePath(langToPathMapping, file)
+    )
+    console.log(file + ' -> ' + descFile)
+    fsExtra.copy(join(root, file), descFile)
   })
 }
 
 // copy userConfig.includes (default to ['src']) *.md to .temp
 // dest file is frontmatter.map?.path or real path at .temp
-async function copySrc(root: string, srcIncludes: string[]) {
+async function copySrc(
+  root: string,
+  srcIncludes: string[],
+  langToPathMapping: Record<string, string> | null
+) {
   const srcPaths = srcIncludes.map((src) => `${clearSuffix(src)}/**/*.md`)
   const files = await globby(srcPaths, {
     dot: true,
@@ -62,11 +86,61 @@ async function copySrc(root: string, srcIncludes: string[]) {
         ...frontmatter.map,
         realPath: file
       }
-      const destFile = join(root, TempFileName, destPath)
+      const destFile = join(
+        root,
+        TempFileName,
+        tolocalePath(langToPathMapping, destPath)
+      )
+
+      console.log(destFile)
 
       await fsExtra.ensureFile(destFile)
       await fsExtra.writeFile(destFile, matter.stringify(content, frontmatter))
     })
   )
   console.log('copy done.')
+}
+
+// resolve a mapping from localeConfig
+// like { 'zh-CN: '/', 'en-US': '/en/', '': '/' }
+function getLangToPathMapping(
+  locale: Record<string, LocaleConfig> | undefined,
+  lang: string | undefined
+) {
+  if (!locale) {
+    return null
+  }
+  const mapping = Object.entries(locale).reduce((map, [path, localeConfig]) => {
+    map[localeConfig.lang] = path.slice(1)
+    return map
+  }, {} as Record<string, string>)
+
+  // ensure default lang
+  let defaultLangPrefix
+  if (lang && mapping[lang]) {
+    defaultLangPrefix = mapping[lang]
+  } else {
+    defaultLangPrefix = Object.values(mapping)[0]
+  }
+  mapping[''] = defaultLangPrefix
+  console.log(mapping)
+  return mapping
+}
+
+// resolve /comp/foo.zh-CN.md -> /zh/comp/foo.md
+function tolocalePath(mapping: Record<string, string> | null, path: string) {
+  if (!mapping) {
+    return path
+  }
+  const fileName = basename(path)
+  const ext = extname(fileName)
+  console.log('fileName = ' + fileName)
+  console.log('ext = ' + ext)
+  // .zh-CN
+  const lang = extname(fileName.slice(0, -ext.length))
+  console.log('lang = ' + lang)
+  const langPrefix = mapping[lang.slice(1)]
+  console.log(`${langPrefix}${path.slice(0, -(ext + lang).length)}${ext}`)
+  console.log('\n')
+  return `${langPrefix}${path.slice(0, -(ext + lang).length)}${ext}`
 }
