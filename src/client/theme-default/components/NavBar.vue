1a3ca9cc897b42066f216e4349a7c12fe0e0c5b2
zuofenghua <11100776@bbktel.com>
2021-10-09 10:21:38 +0800

    fix: update vue

--- src/client/theme-default/components/NavBar.vue
+++ src/client/theme-default/components/NavBar.vue
@@ -15,12 +15,11 @@
 </template>
 
 <script setup lang="ts">
-import { defineEmit } from 'vue'
 import NavBarTitle from './NavBarTitle.vue'
 import NavLinks from './NavLinks.vue'
 import ToggleSideBarButton from './ToggleSideBarButton.vue'
 
-defineEmit(['toggle'])
+defineEmits(['toggle'])
 </script>
 
 <style scoped>
@@ -34,14 +33,14 @@ defineEmit(['toggle'])
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid var(--c-divider);
-  padding: .7rem 1.5rem .7rem 4rem;
+  padding: 0.7rem 1.5rem 0.7rem 4rem;
   height: var(--header-height);
   background-color: #ffffff;
 }
 
 @media (min-width: 720px) {
   .nav-bar {
-    padding: .7rem 1.5rem;
+    padding: 0.7rem 1.5rem;
   }
 }
 
